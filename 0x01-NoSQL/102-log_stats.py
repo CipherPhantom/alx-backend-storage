3fac2b503a9d253d62b0352957fb4f7e185a0e88
CipherPhantom <oyepitan10@gmail.com>
2024-01-24 00:11:16 +0100

    Make Minor Changes

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -2,37 +2,32 @@
 """
 This script provides some stats about Nginx logs stored in MongoDB
 """
-import pymongo
+from pymongo import MongoClient
 
-
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 if __name__ == "__main__":
 
-    client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
-    nginx_collection = None
-    if "logs" in client.list_database_names() and \
-            "nginx" in client.logs.list_collection_names():
-        nginx_collection = client.logs.nginx
-    if nginx_collection is not None:
-        print("{} logs".format(nginx_collection.count_documents({})))
-        print("Methods:")
-        for method in methods:
-            print("\tmethod {}: {}".format(
-                method,
-                nginx_collection.count_documents({"method": method}))
-                )
-        print("{} status check".format(
-            nginx_collection.count_documents({
-                "method": "GET",
-                "path": "/status"
-                })))
-        pipeline = [
-            {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-            {"$sort": {"count": -1}},
-            {"$limit": 10}
-            ]
-        result = list(nginx_collection.aggregate(pipeline))
-        print("IPs:")
-        for entry in result:
-            print("\t{}: {}".format(entry["_id"], entry["count"]))
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    nginx_collection = client.logs.nginx
+    print("{} logs".format(nginx_collection.count_documents({})))
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        print("\tmethod {}: {}".format(
+            method,
+            len(list(nginx_collection.find({"method": method}))))
+            )
+    print("{} status check".format(
+        len(list(nginx_collection.find({
+            "method": "GET",
+            "path": "/status"
+            })))))
+    pipeline = [
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ]
+    result = list(nginx_collection.aggregate(pipeline))
+    print("IPs:")
+    for entry in result:
+        print("\t{}: {}".format(entry["_id"], entry["count"]))
--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,28 +2,23 @@
 """
 This script provides some stats about Nginx logs stored in MongoDB
 """
-import pymongo
+from pymongo import MongoClient
 
-
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 if __name__ == "__main__":
 
-    client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
-    nginx_collection = None
-    if "logs" in client.list_database_names() and \
-            "nginx" in client.logs.list_collection_names():
-        nginx_collection = client.logs.nginx
-    if nginx_collection is not None:
-        print("{} logs".format(nginx_collection.count_documents({})))
-        print("Methods:")
-        for method in methods:
-            print("\tmethod {}: {}".format(
-                method,
-                nginx_collection.count_documents({"method": method}))
-                )
-        print("{} status check".format(
-            nginx_collection.count_documents({
-                "method": "GET",
-                "path": "/status"
-                })))
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    nginx_collection = client.logs.nginx
+    print("{} logs".format(nginx_collection.count_documents({})))
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        print("\tmethod {}: {}".format(
+            method,
+            len(list(nginx_collection.find({"method": method}))))
+            )
+    print("{} status check".format(
+        len(list(nginx_collection.find({
+            "method": "GET",
+            "path": "/status"
+            })))))
