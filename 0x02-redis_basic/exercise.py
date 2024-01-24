ca64f2e551d97cc63d4b3af7e8d0cd17bfb1c5d0
CipherPhantom <oyepitan10@gmail.com>
2024-01-24 18:52:28 +0100

    Complete Mandatory Task

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,6 +19,23 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """Decorator Function"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper Function"""
+        input_key = "{}:inputs".format(method.__qualname__)
+        output_key = "{}:outputs".format(method.__qualname__)
+
+        output = method(self, *args, **kwargs)
+
+        self._redis.rpush(input_key, str(args))
+        self._redis.rpush(output_key, output)
+
+        return output
+    return wrapper
+
+
 class Cache:
     """Represents a cache"""
     def __init__(self):
@@ -27,6 +44,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generates a random key
 
@@ -54,3 +72,30 @@ class Cache:
     def get_int(self, key: str) -> Union[int, None]:
         """Gets and convert the data an int data"""
         return self.get(key, fn=int)
+
+
+def replay(method: Callable) -> None:
+    """Displays the history of calls of a particular function"""
+    if method is None or not hasattr(method, '__self__'):
+        return
+    redis_store = getattr(method.__self__, '_redis', None)
+    if not isinstance(redis_store, redis.Redis):
+        return
+
+    key = method.__qualname__
+    input_key = "{}:inputs".format(key)
+    output_key = "{}:outputs".format(key)
+    count = 0
+
+    if redis_store.exists(key) != 0:
+        count = int(redis_store.get(key))
+    inputs = redis_store.lrange(input_key, 0, -1)
+    outputs = redis_store.lrange(output_key, 0, -1)
+
+    print("{} was called {} times:".format(key, count))
+    for ins, outs in list(zip(inputs, outputs)):
+        print("{}(*{}) -> {}".format(
+            key,
+            ins.decode("utf-8"),
+            outs.decode("utf-8")
+            ))
